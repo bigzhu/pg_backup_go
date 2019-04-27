91f4bfa0054fff3f110b24d546bd73e4f6d959ff
bigzhu <vermiliondun@gmail.com>
2019-04-27 15:57:26 +0800

    ignore toml

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/bigzhu/pg_backup_go
 
 go 1.12
 
-require github.com/bigzhu/gobz v0.0.0-20190427035733-16e303e85415 // indirect
+require github.com/bigzhu/gobz v0.0.0-20190427035733-16e303e85415
--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"os"
 	"os/exec"
 
 	"github.com/bigzhu/gobz/confbz"
 )
 
+const tool string = "pg_dump"
+
 func checkLsExists() error {
-	path, err := exec.LookPath("pg_dump")
+	path, err := exec.LookPath(tool)
 	if err != nil {
 		fmt.Printf("didn't find 'pg_dump' executable, you need install postgresql first\n")
 	} else {
@@ -24,4 +28,26 @@ func main() {
 	}
 	dbConf := confbz.GetDBConf()
 	fmt.Println(dbConf)
+	password := fmt.Sprintf(`export PGPASSWORD="%s";`, dbConf.Password)
+	host := fmt.Sprintf("--host=%s", dbConf.Host)
+	port := fmt.Sprintf("--port=%s", dbConf.Port)
+	user := fmt.Sprintf("--username=%s", dbConf.User)
+	params := "--format=c"
+	file := fmt.Sprintf("--file=%s", "test.dump")
+
+	dbName := fmt.Sprintf("-U %s", dbConf.DBName)
+	cmd := exec.Command(tool, host, port, user, params, file, dbName)
+	cmd.Env = append(os.Environ(),
+		password,
+	)
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err = cmd.Run()
+
+	if err != nil {
+		// log.Fatal(err)
+	}
+	outStr, errStr := string(stdout.Bytes()), string(stderr.Bytes())
+	fmt.Printf("out:\n%s\nerr:\n%s\n", outStr, errStr)
 }
